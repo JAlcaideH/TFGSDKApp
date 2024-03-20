8a9ba210a3ce998003617f4801363627c595da67
Jorge <jorge.alcaide@vodafone.com>
2024-03-20 17:08:40 +0100

    Servicio actualizado incluyendo api de mapBox

--- app/src/main/java/com/example/tfgsdkapp/Service.kt
+++ app/src/main/java/com/example/tfgsdkapp/Service.kt
@@ -1,14 +1,22 @@
 package com.example.tfgsdkapp
 
-import com.google.android.material.snackbar.Snackbar
 import com.vodafone.v2x.sdk.android.facade.V2XSDK
 import com.vodafone.v2x.sdk.android.facade.events.EventCamListChanged
 import kotlin.math.pow
 import com.google.maps.GeoApiContext
 import com.google.maps.DirectionsApi
 import com.google.maps.model.TravelMode
+import com.mapbox.api.directions.v5.DirectionsCriteria
+import com.mapbox.api.directions.v5.MapboxDirections
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.geojson.Point
+import com.vodafone.v2x.sdk.android.core.messages.cpm_pdu_descriptions.CpmManagementContainer._stationType
+import retrofit2.Response
+import java.util.Properties
 
 class Service {
+
+    private var contador = 0
 
     private fun calcularDistancia(lat1: Float, lon1: Float, lat2: Float, lon2: Float): Double {
         val radioTierra = 6371e3 // Radio de la Tierra en metros
@@ -25,19 +33,47 @@ class Service {
         return radioTierra * c // Devuelve la distancia en metros
     }
 
-    private fun estanCerca(lat1: Float, lon1: Float, lat2: Float, lon2: Float): Boolean {
+    private fun estanCerca(lat1: Float, lon1: Float, lat2: Float, lon2: Float, station: Long): Boolean {
         val distancia = calcularDistancia(lat1, lon1, lat2, lon2)
+        if(distancia > 120){
+            return false
+        }
+        if(listaDistancias.size <= 1) {
+            contador ++
+            listaDistancias.add(stationConDist(station,distancia))
+            return false
+        } else {
+            if (contador % 2 == 0) {
+                val segundoElemento = listaDistancias.removeAt(1)
+                listaDistancias[0] = segundoElemento
+                listaDistancias.add(stationConDist(station, distancia))
+            }
+            contador++
+        }
+        if ((listaDistancias.size == 2) && (!(listaDistancias.get(0).station == listaDistancias.get(1).station) || !(listaDistancias.get(0).dist > listaDistancias.get(1).dist)) ) {
+            return false
+        }
         println("Distancia actual: $distancia")
-        return distancia <= 50 // Comprueba si la distancia es menor o igual a 100 metros
+        return true
     }
 
-    //Comparamos si la velocidad de otro dispositivo es mayor a la mia
-    private fun compareVelocity(vel1: Float, vel2: Float): Boolean {
-        return vel2 > vel1
+    private fun compararVelocidad(vel1: Float, vel2: Float): Boolean {
+        return vel2 > 20.0 + vel1
     }
 
+    private fun estacionNoValida(stationTypeInput: Int): Boolean {
+
+        val estacionesValidas = setOf(_stationType.passengerCar, _stationType.bus, _stationType.lightTruck,
+            _stationType.heavyTruck,_stationType.trailer,_stationType.specialVehicles)
+
+        return !estacionesValidas.contains(stationTypeInput)
+    }
+
+    data class stationConDist(val station: Long, val dist: Double)
+    val listaDistancias = mutableListOf<stationConDist>()
 
     fun compararPosiciones(eventCamListChanged: EventCamListChanged) : Int {
+        val startTime = System.currentTimeMillis()
         val camRecords = eventCamListChanged.list
         val myRecord =
             camRecords.firstOrNull { it.stationID == V2XSDK.getInstance().sdkConfiguration.stationID }
@@ -45,17 +81,28 @@ class Service {
 
         if (myRecord != null) {
             for (record in camRecords) {
+                if(estacionNoValida(record.stationType)) {
+                    continue
+                }
                 if (record != myRecord) {
-                    if(compareVelocity(myRecord.speedInKmH,record.speedInKmH)) {
+                    if(compararVelocidad(myRecord.speedInKmH,record.speedInKmH)) {
                         val direccion = compararAnguloDir(myRecord.headingInDegree,record.headingInDegree)
                         if (direccion != 0) {
-                            if (estanCerca(myRecord.latitude, myRecord.longitude, record.latitude, record.longitude)) {
-                                //atLeastOneNear = direccion
-                                if(calcularDistanciaCarretera(myRecord.latitude, myRecord.longitude, record.latitude, record.longitude) < 50) {
+                            if (estanCerca(myRecord.latitude, myRecord.longitude, record.latitude, record.longitude,record.stationID)) {
+                                /*if(calcularDistanciaCarretera(myRecord.latitude, myRecord.longitude, record.latitude, record.longitude) < 50) {
                                     println("Velocidad actual: ${record.speedInKmH}")
-                                    println("Distancia en carretera: ${calcularDistanciaCarretera(myRecord.latitude, myRecord.longitude, record.latitude, record.longitude)}")
+                                    atLeastOneNear = direccion
+                                }*/
+                                if(calculateDistanceMapBox(myRecord.latitude, myRecord.longitude, record.latitude, record.longitude) < 120) {
+                                    println("Velocidad actual: ${record.speedInKmH}")
                                     atLeastOneNear = direccion
                                 }
+                                //atLeastOneNear = direccion
+                                val endTime = System.currentTimeMillis()
+                                val elapsedTime = endTime - startTime
+                                println("El proceso tardó $elapsedTime milisegundos.")
+                            } else {
+                                continue
                             }
                         }
                     }
@@ -65,12 +112,6 @@ class Service {
         return atLeastOneNear
     }
 
-    private fun compararAngulo(myAngulo: Float, otroAngulo: Float) : Boolean {
-        val diff = (otroAngulo - myAngulo + 360) % 360
-        val diffNormalized = if (diff > 180) diff - 360 else diff
-        println("angulo actual: ${diffNormalized}")
-        return diffNormalized in -100f..100f
-    }
     private fun compararAnguloDir(myAngulo: Float, otroAngulo: Float) : Int {
         var diff = otroAngulo - myAngulo
         if (diff < 0) {
@@ -91,12 +132,30 @@ class Service {
         }
     }
 
+    private fun calculateDistanceMapBox(lat1: Float, lon1: Float, lat2: Float, lon2: Float): Float {
 
-    //Distancia desde el punto externo hasta mi posicion, por carretera
+        var responseDist: Float = 0F
+        val client = MapboxDirections.builder()
+            .origin(Point.fromLngLat(lon2.toDouble(),lat2.toDouble()))
+            .destination(Point.fromLngLat(lon1.toDouble(),lat1.toDouble()))
+            .profile(DirectionsCriteria.PROFILE_DRIVING)
+            .accessToken(BuildConfig.apikeyMapBox)
+            .build()
+
+        val response: Response<DirectionsResponse> = client.executeCall()
+        return if(response != null) {
+            responseDist = response.body()?.routes()?.firstOrNull()?.distance()?.toFloat() ?: 1000F
+            println("Distanc actual MapBox: ${responseDist}")
+            responseDist
+        } else {
+            1000F
+        }
+    }
+
     private fun calcularDistanciaCarretera(lat1: Float, lon1: Float, lat2: Float, lon2: Float): Float {
-        val apiKey = "AIzaSyAP8GhkfWaCV2XWWt5sXGWzzlT5fx1uIHM"
+
         val context = GeoApiContext.Builder()
-            .apiKey(apiKey)
+            .apiKey(BuildConfig.apikeyGoogle)
             .build()
 
         val directionsResult = DirectionsApi.newRequest(context)
@@ -107,6 +166,7 @@ class Service {
 
         val distanciaEnCarretera = directionsResult.routes[0].legs[0].distance.inMeters.toFloat()
 
+        println("Distanc actual GoogleMaps: ${distanciaEnCarretera}")
         return distanciaEnCarretera
     }
 }
